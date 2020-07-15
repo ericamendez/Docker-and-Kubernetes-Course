5b7f6acf635cf5e9167084795d49e2e03cde29fb
ericamendez <ecmendez25@gmail.com>
2020-07-15 18:52:08 -0400

    docker-compose with multiple local containers

--- Dockerfile
+++
@@ -1,1 +0,0 @@
-# This Dockerfile only has to do with setting up our Node container, not the redis container
--- visits/index.js
+++ visits/index.js
@@ -4,11 +4,25 @@ const redis = require('redis');
 // make an instance of express
 const app = express();
 
-const client = redis.createClient();
+
+const client = redis.createClient({
+    /**  
+     * specify where the server is running.
+     * if we were not using docker we would put in some type of address/connection url. ex) 'https://my-redis-server.com'
+     * 
+     * since we are using docker compose, we can refer to the the redis container also running by name.
+     * express & redis have no idea what 'rdeis-server' means, redis takes string and has faith its a meaningful URL
+     * when connection request goes out from node-app, docker will see it and see it's trying to accesss a host and redirect it to the other container named 'redis-server'
+    */
+    host: 'redis-server',
+    // can specify port, by default port used with reddit is 6379
+    port: 6379
+});
+
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
-    cllient.get('visits', (err, visits) => {
+    client.get('visits', (err, visits) => {
         res.send('Number of visits is ' + visits);
         client.set('visits', parseInt(visits) + 1);
     })
