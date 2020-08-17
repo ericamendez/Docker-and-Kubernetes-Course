626e3704275da99d99cb2e5d70a502f14a53319c
ericamendez <ecmendez25@gmail.com>
2020-08-17 17:22:07 -0400

    can't connect to postgres (Bad Request)

--- complex/server/index.js
+++ complex/server/index.js
@@ -1,86 +1,71 @@
-const keys = require('./keys')
+const keys = require('./keys');
 
+// Express App Setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-// EXPRESS APP SETUP
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-const app = express()
-// cors allow us to make request from one domain (react app will be running on) to a completely different domain where the express API is hosted on
-app.use(cors())
-app.use(bodyParser.json())
-
-
-// POSTGRES CLIENT SETUP
+// Postgres Client Setup
 const { Pool } = require('pg');
 const pgClient = new Pool({
-    user: keys.pgUser,
-    host: keys.pgHost,
-    database: keys.pgDatabase,
-    password: keys.pgPassword,
-    port: keys.pgPort
-})
-
-// pgClient.on('error', () => console.log('Lost PG connection'))
-
-// // we have to initially create at least one time a table that will  store all the values
-// pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
-//     .catch (err => console.log(err))
-
-// Update to Ensure that we delay the table query until after a connection is made.
-pgClient.on('connect', () => {
-    pgClient
-        .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-        .catch((err) => console.log(err));
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
 });
 
+pgClient.on('connect', () => {
+  pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch((err) => console.log(err));
+});
 
-//  REDIS CLIENT SETUP
-const redis = require('redis')
+// Redis Client Setup
+const redis = require('redis');
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
-})
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
 const redisPublisher = redisClient.duplicate();
 
+// Express route handlers
 
-// EXPRESS ROUTE HANDLERS
 app.get('/', (req, res) => {
-    res.send('Hi')
-})
+  res.send('Hi');
+});
 
 app.get('/values/all', async (req, res) => {
-    const values = await pgClient.query('SELECT * FROM values')
+  const values = await pgClient.query('SELECT * from values');
 
-    res.send(values.rows);
-})
+  res.send(values.rows);
+});
 
 app.get('/values/current', async (req, res) => {
-    redisClient.hgetall('values', (err,values) => {
-        res.send(values)
-    })
-})
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
+});
 
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
 
-// route handler to receive new values from React Appliucation
-app.post('/values', async (req,res) => {
-    const index = req.body.index
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
+  }
 
-    if (parseInt(index) > 40) {
-        return res.status(422).send('Index too high!')
-    }
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
-    // put value into redis data store, eventually worker will go into hash and replace string with value
-    redisClient.hset('values', index, 'Nothing yet!')
-    // message that gets send over to the worker process and to pull new number out of redis and calculate new number for it
-    redisPublisher.publish('insert', index)
-    pgClient.query('INSERT INTO values(number) VALUES($1)', [index])
+  res.send({ working: true });
+});
 
-    res.send({ working: true })
-})
-
-app.listen(5000, err => {
-    console.log('Listening')
-})
+app.listen(5000, (err) => {
+  console.log('Listening');
+});
